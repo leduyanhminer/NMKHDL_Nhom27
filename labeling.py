62e3d732afdf33c4e79a3a01868d5ffb01579f07
Nosa <leduyanhminer@gmail.com>
2024-01-08 02:05:58 +0700

    update label

--- labeling.py
+++ labeling.py
@@ -1,37 +1,36 @@
 from pymongo import MongoClient
-import random
+import pandas as pd
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import *
+from bson import ObjectId
 
 client = MongoClient(DB_URI) 
 db = client[DB_NAME] 
 collection = db[DB_COLLECTION_NAME]
 
-def random_binary():
-    return random.randint(0, 1)
+df = pd.read_csv('model/laptop_final.csv')
 
-for document in collection.find():
-    collection.update_one(
-        {'_id': document['_id']},
-        {'$set': {
-            'gaming': random_binary(), 
-            'lapTrinh': random_binary(), 
-            'vanPhong': random_binary(), 
-            'doHoa': random_binary(), 
-            'doanhNhan': random_binary()
-        }}
-    )
-
-
-# result = collection.update_many(
+# collection.update_many(
 #     {},
-#     { '$unset': {
-#         'gaming': "", 
-#         'lapTrinh': "", 
-#         'vanPhong': "", 
-#         'doHoa': "", 
-#         'doanhNhan': ""
+#     {'$set': {
+#         'gaming': None, 
+#         'lapTrinh': None, 
+#         'vanPhong': None, 
+#         'doHoa': None, 
+#         'doanhNhan': None
 #     }}
 # )
+
+for index, row in df.iterrows():
+    collection.update_one(
+        {'_id': ObjectId(row['_id'])},
+        {'$set': {
+            'gaming': row['Game'], 
+            'lapTrinh': row['Lap Trinh'], 
+            'vanPhong': row['Van Phong'], 
+            'doHoa': row['Do Hoa'], 
+            'doanhNhan': row['Doanh Nhan']
+        }}
+    )
